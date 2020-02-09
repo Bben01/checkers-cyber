2075f8ca9a807f2c570b5fb57b3985c5b2026e9e
Bben01 <52465698+Bben01@users.noreply.github.com>
2020-02-09 16:50:06 +0200

    Added camera movement
    Ability to control the camera with the mouse when playing the game

---
+++ Checkers/Assets/SceneChanger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SceneChanger : MonoBehaviour
+{
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadeToLevel(1);
+            Debug.Log("OK");
+        }
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        animator.SetTrigger("NextScene");
+    }
+}
